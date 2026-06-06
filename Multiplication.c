1bcddcf0fd65cf4c86a3e2999933a1cda2d438be
H A M I M <abhamim740@gmail.com>
2026-06-06 21:04:54 +0600

    Add files via upload

---
+++ Multiplication.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int i,y;
+    printf("Multiplication:\n");
+    scanf("%d",&y);
+    {
+    for(i=1;i<=10;i++)
+    printf("%d x %d = %d\n",y,i,y*i);
+    }
+    return 0;
+
+}
