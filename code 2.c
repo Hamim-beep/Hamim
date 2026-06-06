6e771ea557aacd47072cb2ecd82e8317005fcc01
H A M I M <abhamim740@gmail.com>
2026-06-06 20:53:56 +0600

    Add files via upload

---
+++ code 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y, sum;
+
+    scanf("%d%d", &x, &y);
+
+    printf("The sum of integers:\n");
+
+    sum = x + y;
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
