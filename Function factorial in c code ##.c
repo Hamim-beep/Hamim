41aa3ccb4288e51b4f4c8378c9fa3d792bf1f4e1
H A M I M <abhamim740@gmail.com>
2026-06-13 22:04:10 +0600

    Add files via upload

---
+++ Function factorial in c code ##.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int fact(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * fact(n - 1);
+    }
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter the value of n:\n ");
+    scanf("%d", &num);
+
+    int factorial = fact(num);
+
+    printf("%d! = %d\n", num, factorial);
+
+    return 0;
+}
---
+++ Function fibonacci in c  code ##.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int fib(int n)
+{
+    if (n == 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+    else
+        return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter the value of n:\n ");
+    scanf("%d", &num);
+
+    printf("Fibonacci(%d) = %d\n", num, fib(num));
+
+    return 0;
+}
